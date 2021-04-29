657ec2fe323e38cfc075eb0741a6098cc4bb17d6
Chad Stein <chadbstein@Chads-MacBook-Pro.local>
2021-04-28 20:04:17 -0400

    Added About page and started to make manufacturer-level data tab

--- app.R
+++ app.R
@@ -18,7 +18,49 @@ library(rjson)
 # Define UI for application that draws a histogram
 ui <- navbarPage("COVID-19 Vaccination Dashboard",
     
-    tabPanel("National Trends",
+    tabPanel("About",
+             fluidRow(
+                 column(8,
+                        h2(p("About this project:")),
+                        h6(p("This project aggregates data from the Centers for Disease Control and Prevention (CDC) about the ongoing COVID-19 pandemic.")), 
+                        h6(p("It was designed and executed by Chad Stein and Lily Payvandi as part of the BMI 706 final project for Spring 2021.")),
+                        h6(p("Weekly vaccination data updates automatically."))))),
+                     
+
+    tabPanel("Trends by Manufacturer",
+             
+             sidebarLayout(
+                 sidebarPanel(
+                     radioButtons("brand",
+                                  h3("Select a manufacturer:"),
+                                  choices= list("Moderna" = 'Moderna',
+                                                "Pfizer/BioNTech" = 'Pfizer',
+                                                "Johnson and Johnson" = 'Johnson')),
+                     
+                     sliderInput("Dates",
+                                 "Dates:",
+                                 min = as.Date(oldestDate,"%Y-%m-%d"),
+                                 max = as.Date(mostRecentDate,"%Y-%m-%d"),
+                                 value=c(as.Date(oldestDate,"%Y-%m-%d"), as.Date(mostRecentDate,"%Y-%m-%d")),
+                                 timeFormat="%Y-%m-%d")
+                     
+                 ),
+                 
+                 
+                 
+                 # Show a plot
+                 mainPanel(
+                     plotlyOutput("vaccineMap"),
+                     verbatimTextOutput("instructions"),
+                     plotlyOutput("vaccineTimeline")
+                     
+                 )
+             )
+    ),
+    
+    
+    
+    tabPanel("Cross-manufacturer Trends",
              
              sidebarLayout(
                  sidebarPanel(
@@ -41,56 +83,19 @@ ui <- navbarPage("COVID-19 Vaccination Dashboard",
                  # Show a plot
                  mainPanel(
                      plotlyOutput("map"),
-                     #plotlyOutput("timeline"),
-                     #verbatimTextOutput("click"),
-                     #verbatimTextOutput("columns"),
                      plotlyOutput("timeline")
                  )
              )
-    ),
-    
-    tabPanel("Trends by manufacturer",
-             
-             sidebarLayout(
-                 sidebarPanel(
-                     radioButtons("brand2",
-                                  h3("Select a manufacturer, or look at national trends:"),
-                                  choices= list("Moderna only" = 'Moderna',
-                                                "Pfizer/BioNTech only" = 'Pfizer',
-                                                "Johnson and Johnson/Janssen only" = 'Janssen')),
-                     
-                     radioButtons("delivery2",
-                                  h3("Status of vaccine"),
-                                  choices= list("Delivered" = 'delivered',
-                                                "Administered" = 'administered')),
-                     
-                     radioButtons("age2",
-                                  h3("Age"),
-                                  choices= list("All age groups" = 'Total',
-                                                "18-65" = '18plus',
-                                                "65+" = '65plus'))         
-                     
-                 ),
-                 
-                 
-                 
-                 # Show a plot
-                 mainPanel(
-                     plotlyOutput("map2"),
-                     #plotlyOutput("timeline"),
-                     #verbatimTextOutput("click"),
-                     #verbatimTextOutput("columns"),
-                     tableOutput("age2")
-                 )
-             )
     )
-    
     
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
+    
+    oldestDate <- as.Date(min(johnsonTimeline$Week, modernaTimeline$Week, pfizerTimeline$Week))
+    mostRecentDate <- as.Date(max(johnsonTimeline$Week, modernaTimeline$Week, pfizerTimeline$Week))
+    
     output$map <- renderPlotly({
         
 
@@ -158,6 +163,102 @@ server <- function(input, output) {
         timelineplot
         
     })
+    
+    output$vaccineMap <- renderPlotly({
+        
+    
+        
+        if(input$brand == "Moderna"){
+            vaccineDF <- modernaTimeline
+        } else if(input$brand == "Pfizer"){
+            vaccineDF <- pfizerTimeline
+        } else if(input$brand == "Johnson"){
+            vaccineDF <- johnsonTimeline
+        }
+        
+        
+        filteredData <- vaccineDF %>% 
+            filter(Week >= input$Dates[1]) %>% filter(Week <= input$Dates[2]) %>%
+            group_by(iso3166_2) %>%
+            summarise(`FirstDoseAllocation` = sum(`FirstDoseAllocation`))
+        
+        
+        g <- list(
+            fitbounds = "locations",
+            visible = FALSE
+        )
+        
+        fig2 <- plot_ly()
+        
+        fig2 <- fig2 %>% add_trace(
+            type="choropleth",
+            geojson=states,
+            locations=filteredData$iso3166_2,
+            z=filteredData$FirstDoseAllocation,
+            colorscale="Blues", reversescale=TRUE,
+            featureidkey="properties.iso3166_2")
+        
+        fig2 <- fig2 %>% layout(
+            geo = g,
+            title = "Vaccinations by state"
+        )
+    
+        
+        
+        fig2
+        
+    })
+    
+    output$timeline <- renderPlotly({
+        
+        if(input$age == "Total"){
+            selectedAgeForTimeLine <- agnosticTimeLineGroupedAll_Total
+        } else if(input$age == "18plus"){
+            selectedAgeForTimeLine <- agnosticTimeLineGroupedAll %>% filter(betterAgeGroup == "18plus")
+        } else if(input$age == "65plus"){
+            selectedAgeForTimeLine <- agnosticTimeLineGroupedAll %>% filter(betterAgeGroup == "65plus")
+        }
+        
+        timelineplot <- plot_ly(data = selectedAgeForTimeLine, x=~Date)
+        
+        timelineplot <- timelineplot %>% add_trace(y = ~`Percent of age group with at least one dose`, 
+                                                   name = '`At least one dose', 
+                                                   mode = 'lines+markers')
+        timelineplot <- timelineplot %>% add_trace(y = ~`Percent of age group fully vaccinated`, 
+                                                   name = '`Fully Vaccinated', 
+                                                   mode = 'lines+markers')
+        timelineplot <- timelineplot %>% layout(title = "Vaccinations over time",
+                                                xaxis = list(title = "Week"),
+                                                yaxis = list (title = "Percent of people", range = c(0, 100)))
+        timelineplot
+        
+    })
+    
+    output$vaccineTimeline <- renderPlotly({
+        
+        if(input$brand == "Moderna"){
+            vaccineDF <- modernaTimeline
+        } else if(input$brand == "Pfizer"){
+            vaccineDF <- pfizerTimeline
+        } else if(input$brand == "Johnson"){
+            vaccineDF <- johnsonTimeline
+        }
+        
+        filteredDataTimeLine <- vaccineDF %>% 
+            group_by(iso3166_2)
+        
+        vaccineTimelinePlot <- plot_ly(data=filteredDataTimeLine)
+        
+        vaccineTimelinePlot <- vaccineTimelinePlot %>% add_trace(x=~Week, y=~FirstDoseAllocation, 
+                                   name = 'FirstDoseAllocation', 
+                                   mode = 'lines',
+                                   hovertemplate = ~iso3166_2)     
+            
+        vaccineTimelinePlot
+        
+        
+    })
+    
 }
 
 # Run the application 
