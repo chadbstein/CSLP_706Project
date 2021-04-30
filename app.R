ec94bd3f82a42b54d883f208814adc261518250f
Chad Stein <chadbstein@chads-mbp.private.wireless.med.harvard.edu>
2021-04-30 12:23:30 -0400

    Added additional filters, started working on highlight function, and added a stacked barchart

--- app.R
+++ app.R
@@ -15,7 +15,17 @@ library(plotly)
 library(ggplot2)
 library(rjson)
 
-# Define UI for application that draws a histogram
+pfizerTimeline<- read_rds("data/pfizerTimeline.rds")
+modernaTimeline<- read_rds("data/modernaTimeline")
+johnsonTimeline <- read_rds("data/johnsonTimeline")
+
+
+oldestDate <- as.Date(min(johnsonTimeline$Week, modernaTimeline$Week, pfizerTimeline$Week))
+mostRecentDate <- as.Date(max(johnsonTimeline$Week, modernaTimeline$Week, pfizerTimeline$Week))
+
+hexes <- "data/us_states_hexgrid.geojson"
+states <- rjson::fromJSON(file=hexes)
+
 ui <- navbarPage("COVID-19 Vaccination Dashboard",
     
     tabPanel("About",
@@ -38,28 +48,38 @@ ui <- navbarPage("COVID-19 Vaccination Dashboard",
                                                 "Johnson and Johnson" = 'Johnson')),
                      
                      sliderInput("Dates",
-                                 "Dates:",
+                                 "Select dates:",
                                  min = as.Date(oldestDate,"%Y-%m-%d"),
                                  max = as.Date(mostRecentDate,"%Y-%m-%d"),
                                  value=c(as.Date(oldestDate,"%Y-%m-%d"), as.Date(mostRecentDate,"%Y-%m-%d")),
-                                 timeFormat="%Y-%m-%d")
+                                 timeFormat="%Y-%m-%d"),
                      
-                 ),
+                     sliderInput("Population",
+                                 "Filter by states with selected population:",
+                                  min = min(pfizerTimeline$PopulationEstimate),
+                                  max = max(pfizerTimeline$PopulationEstimate),
+                                  value=c(min(pfizerTimeline$PopulationEstimate), max(pfizerTimeline$PopulationEstimate))),
+                     
                  
+                    sliderInput("Spending",
+                                "Filter by states with selected public health spending per capita:",
+                                min = min(pfizerTimeline$HealthSpendingPerCapita),
+                                max = max(pfizerTimeline$HealthSpendingPerCapita),
+                                pre = "$", sep = ",",
+                                value=c(min(pfizerTimeline$HealthSpendingPerCapita), max(pfizerTimeline$HealthSpendingPerCapita)))),
                  
+           
                  
                  # Show a plot
                  mainPanel(
                      plotlyOutput("vaccineMap"),
                      verbatimTextOutput("instructions"),
-                     plotlyOutput("vaccineTimeline")
+                     plotlyOutput("vaccineTimeline"),
+                     plotlyOutput("stateRanks")
                      
                  )
-             )
-    ),
-    
-    
-    
+             )),
+ 
     tabPanel("Cross-manufacturer Trends",
              
              sidebarLayout(
@@ -78,9 +98,7 @@ ui <- navbarPage("COVID-19 Vaccination Dashboard",
                      
                  ),
                  
-                 
-                 
-                 # Show a plot
+                # Show a plot
                  mainPanel(
                      plotlyOutput("map"),
                      plotlyOutput("timeline")
@@ -90,11 +108,9 @@ ui <- navbarPage("COVID-19 Vaccination Dashboard",
     
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
     
-    oldestDate <- as.Date(min(johnsonTimeline$Week, modernaTimeline$Week, pfizerTimeline$Week))
-    mostRecentDate <- as.Date(max(johnsonTimeline$Week, modernaTimeline$Week, pfizerTimeline$Week))
+
     
     output$map <- renderPlotly({
         
@@ -109,9 +125,7 @@ server <- function(input, output) {
                                                                     contains("administered", ignore.case = TRUE))
             }
             colnames(plotOfChoiceFiltered) <- c("ISOname", "Stat")
-        
-        
-        
+     
         g <- list(
             fitbounds = "locations",
             visible = FALSE
@@ -141,6 +155,7 @@ server <- function(input, output) {
     
     output$timeline <- renderPlotly({
         
+        
         if(input$age == "Total"){
             selectedAgeForTimeLine <- agnosticTimeLineGroupedAll_Total
         } else if(input$age == "18plus"){
@@ -166,7 +181,7 @@ server <- function(input, output) {
     
     output$vaccineMap <- renderPlotly({
         
-    
+        clickedState <- event_data("plotly_click")
         
         if(input$brand == "Moderna"){
             vaccineDF <- modernaTimeline
@@ -177,10 +192,17 @@ server <- function(input, output) {
         }
         
         
+        vaccineDF <- vaccineDF %>% 
+            filter(PopulationEstimate >= input$Population[1]) %>%
+            filter(PopulationEstimate <= input$Population[2]) %>%
+            filter(HealthSpendingPerCapita >= input$Spending[1]) %>%
+            filter(HealthSpendingPerCapita <= input$Spending[2])
+        
         filteredData <- vaccineDF %>% 
             filter(Week >= input$Dates[1]) %>% filter(Week <= input$Dates[2]) %>%
             group_by(iso3166_2) %>%
-            summarise(`FirstDoseAllocation` = sum(`FirstDoseAllocation`))
+            summarise(`TotalDoseAllocationPer100k` = sum(`TotalDoseAllocationPer100k`))
+        
         
         
         g <- list(
@@ -194,16 +216,13 @@ server <- function(input, output) {
             type="choropleth",
             geojson=states,
             locations=filteredData$iso3166_2,
-            z=filteredData$FirstDoseAllocation,
+            z=filteredData$TotalDoseAllocationPer100k,
             colorscale="Blues", reversescale=TRUE,
             featureidkey="properties.iso3166_2")
         
         fig2 <- fig2 %>% layout(
             geo = g,
-            title = "Vaccinations by state"
-        )
-    
-        
+            title = "Vaccinations by state")
         
         fig2
         
@@ -236,6 +255,8 @@ server <- function(input, output) {
     
     output$vaccineTimeline <- renderPlotly({
         
+        clickedState <- event_data("plotly_click")
+        
         if(input$brand == "Moderna"){
             vaccineDF <- modernaTimeline
         } else if(input$brand == "Pfizer"){
@@ -244,20 +265,94 @@ server <- function(input, output) {
             vaccineDF <- johnsonTimeline
         }
         
+        vaccineDF <- vaccineDF %>% 
+            filter(PopulationEstimate >= input$Population[1]) %>%
+            filter(PopulationEstimate <= input$Population[2]) %>%
+            filter(HealthSpendingPerCapita >= input$Spending[1]) %>%
+            filter(HealthSpendingPerCapita <= input$Spending[2])
+        
         filteredDataTimeLine <- vaccineDF %>% 
             group_by(iso3166_2)
         
-        vaccineTimelinePlot <- plot_ly(data=filteredDataTimeLine)
+        vaccineTimelinePlot <- plot_ly(data=filteredDataTimeLine, color=I('darkgray'), source="vaccineTimelinePlot") %>%
+                                event_register('plotly_relayout')
         
-        vaccineTimelinePlot <- vaccineTimelinePlot %>% add_trace(x=~Week, y=~FirstDoseAllocation, 
-                                   name = 'FirstDoseAllocation', 
+        vaccineTimelinePlot <- vaccineTimelinePlot %>% highlight_key() %>% 
+                                   add_trace(x=~Week, y=~TotalDoseAllocationPer100k, 
+                                   name = 'Doses allocated per 100K people', 
                                    mode = 'lines',
-                                   hovertemplate = ~iso3166_2)     
+                                   hovertemplate = ~iso3166_2) %>% layout(
+                                       xaxis = list(range = c(input$Dates[1], input$Dates[2])))
             
         vaccineTimelinePlot
         
         
     })
+    
+    output$stateRanks <- renderPlotly({
+        
+        clickedState <- event_data("plotly_click")
+        
+        if(input$brand == "Moderna"){
+            vaccineDF <- modernaTimeline
+        } else if(input$brand == "Pfizer"){
+            vaccineDF <- pfizerTimeline
+        } else if(input$brand == "Johnson"){
+            vaccineDF <- johnsonTimeline
+        }
+        
+        
+        vaccineDF <- vaccineDF %>% 
+            filter(PopulationEstimate >= input$Population[1]) %>%
+            filter(PopulationEstimate <= input$Population[2]) %>%
+            filter(HealthSpendingPerCapita >= input$Spending[1]) %>%
+            filter(HealthSpendingPerCapita <= input$Spending[2])
+        
+        filteredData <- vaccineDF %>% 
+            filter(Week >= input$Dates[1]) %>% filter(Week <= input$Dates[2]) #%>%
+            #group_by(iso3166_2) %>%
+            #summarise(`TotalDoseAllocationPer100k` = sum(`TotalDoseAllocationPer100k`))
+        
+        
+        barPlot <- plot_ly(filteredData, x=~Week, y=~TotalDoseAllocationPer100k, type='bar', color = ~iso3166_2, colors="red") %>% 
+            layout(barmode='stack', xaxis = list(range = c(input$Dates[1], input$Dates[2]))) %>% hide_legend()
+        barPlot
+        
+    })
+    
+    output$instructions <- renderPrint({
+        #d <- event_data("plotly_click")e
+        
+        #if (is.null(d) == T) return (NULL);
+        currentView <- event_data(event="plotly_relayout", source="vaccineTimelinePlot")
+        if (is.null(currentView) == T) return (NULL) else gsub( " .*$", "", currentView$`xaxis.range[0]`)
+        #newMin <- currentView$`xaxis.range[0]`
+        #newMax <- currentView$`xaxis.range[2]`
+        #if (is.null(d)) "Click events appear here (double-click to clear)" else d
+    })
+    
+    
+    observe({
+        
+        currentView <- event_data(event="plotly_relayout", source="vaccineTimelinePlot")
+        
+        if(is.null(currentView) || is.na(currentView) || identical(currentView, character(0))){
+            newMin <- oldestDate
+            newMax <- mostRecentDate
+            }
+        
+        newMin <- gsub( " .*$", "", currentView$`xaxis.range[0]`)
+        newMax <- gsub( " .*$", "", currentView$`xaxis.range[1]`)
+        
+        updateSliderInput(session, 'Dates', 
+                          min = as.Date(oldestDate,"%Y-%m-%d"),
+                          max = as.Date(mostRecentDate,"%Y-%m-%d"),
+                          value = c(as.Date(newMin,"%Y-%m-%d"), as.Date(newMax,"%Y-%m-%d")), timeFormat="%Y-%m-%d")
+                          
+                          
+    })
+    
+
     
 }
 
